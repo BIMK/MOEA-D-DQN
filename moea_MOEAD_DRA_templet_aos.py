e8908339eea5a771fdd4957f3d30c3059d3bae40
lepecoder <lepecoder@qq.com>
2021-11-10 19:40:02 +0800

    neighborSize

--- moea_MOEAD_DRA_templet_aos.py
+++ moea_MOEAD_DRA_templet_aos.py
@@ -34,7 +34,7 @@ class moea_MOEAD_DRA_templet(ea.MoeaAlgorithm):
         if population.Encoding == 'RI':
             # self.xovOper = RecRL(problem, self.uniformPoint, MAXGEN, self.NIND)
             # self.countOper = RecRL(problem, self.uniformPoint, MAXGEN, self.NIND)
-            self.de_rand_1 = DE_rand_1()
+            # self.de_rand_1 = DE_rand_1()
             # self.countOper = Best_cro(problem, self.uniformPoint, MAXGEN, population.Encoding, population.Field)
             # self.xovSbx = Recsbx()
             # self.countOper = Best_mut(problem, self.uniformPoint, MAXGEN, population.Encoding, population.Field)
@@ -106,15 +106,15 @@ class moea_MOEAD_DRA_templet(ea.MoeaAlgorithm):
         r = FIR.sum()
         # print(r)
         # if self.currentGen % self.learn_interval == 0:
-        # if random.random() < 0.06:
-        # self.countOper.learn(r)
+        if random.random() < 6:
+            self.countOper.learn(r)
         # self.xovOper.learn(r)
         # self.mutOper.learn(r)
 
     def run(self, prophetPop=None):  # prophetPop为先知种群（即包含先验知识的种群）
         self.run_times += 1
         # ==========================初始化配置===========================
-        # self.countOper = RecRL(self.problem, self.uniformPoint, self.MAXGEN, self.NIND)
+        self.countOper = RecRL(self.problem, self.uniformPoint, self.MAXGEN, self.NIND)
         # self.countOper.dqn.eval_net.load_state_dict(torch.load('./igd_desc/UF1_model.pth'))
         self.initialization()
         population = self.population
@@ -155,8 +155,8 @@ class moea_MOEAD_DRA_templet(ea.MoeaAlgorithm):
                     # 实例化一个种群对象用于存储进化的后代（这里只进化生成一个后代）
                     offspring = ea.Population(population.Encoding, population.Field, 1)
                     # offspring.Chrom = self.countOper.do(population.Chrom, i, indices, idealPoint, self.currentGen)  # Best_cro
-                    # offspring.Chrom = self.countOper.do(population.Chrom, i, indices, self.currentGen)  # RL
-                    offspring.Chrom = self.de_rand_1.do(population.Chrom, i, indices)  # de rand 1
+                    offspring.Chrom = self.countOper.do(population.Chrom, i, indices, self.currentGen)  # RL
+                    # offspring.Chrom = self.de_rand_1.do(population.Chrom, i, indices)  # de rand 1
                     # offspring.Chrom = self.xovSbx.do(population.Chrom, i, indices)  # sbx模拟二进制交叉
                     offspring.Chrom = self.mutPolyn.do(offspring.Encoding, offspring.Chrom, offspring.Field)  # 多项式变异
                     # offspring.Chrom = self.mutOper.do(population.Chrom, offspring.Chrom, i, self.currentGen)
--- moea_main.py
+++ moea_main.py
@@ -42,10 +42,10 @@ if __name__ == '__main__':
     # problems = ['ZDT1', 'ZDT2', 'ZDT3', 'ZDT4', 'ZDT6']
     # problems = ['WFG1', 'WFG2', 'WFG3', 'WFG4', 'WFG5', 'WFG6', 'WFG7', 'WFG8', 'WFG9']
     # problems = ['ZDT1', 'ZDT1', 'ZDT1', 'ZDT1']
-    problems = ['NN']
+    problems = ['UF1', 'UF1', 'UF1', 'UF1', 'UF1', 'UF1', 'UF1', 'UF1', ]
 
-    # nrs = [5, 10, 15, 20]
-    N = 1   # 独立运行N次，取中值
+    nrs = [5, 10, 15, 20, 25, 30, 35, 40]
+    N = 5   # 独立运行N次，取中值
     results = list()
     for idx in range(len(problems)):
         # for problemName in problems:
@@ -59,18 +59,19 @@ if __name__ == '__main__':
         PF = problem.getReferObjV()  # 获取真实前沿，详见Problem.py中关于Problem类的定义
         """======================种群设置==============================="""
         Encoding = 'RI'             # 编码方式
-        NIND = 100                  # 种群规模
+        NIND = 600                  # 种群规模
         Field = ea.crtfld(Encoding, problem.varTypes, problem.ranges, problem.borders)  # 创建区域描述器
         # 实例化种群对象（此时种群还没被初始化，仅仅是完成种群对象的实例化）
         population = ea.Population(Encoding, Field, NIND)
         """======================算法参数设置=========================="""
         # myAlgorithm = moea_MOEAD_DE_templet(problem, population)
-        MAXGEN = 100
+        MAXGEN = 500
         myAlgorithm = moea_MOEAD_DRA_templet(problem, population, MAXGEN)
         myAlgorithm.drawing = 0  # 设置绘图方式（0：不绘图；1：绘制结果图；2：绘制目标空间过程动画；3：绘制决策空间过程动画）
         myAlgorithm.verbose = False
         igd = np.empty(N)
         hv = np.empty(N)
+        myAlgorithm.neighborSize = nrs[idx]
         """==========================调用算法模板进行种群进化========================"""
         """
         def f(x):
@@ -88,10 +89,7 @@ if __name__ == '__main__':
         """
         # """
         for i in range(N):
-            start_time = time.time()
             NDSet, pop, plt = myAlgorithm.run()  # 每次run都是独立的，会重新初始化模版的一些动态参数
-            end_time = time.time()
-            print("running time %.4f" % (end_time - start_time))
             IGD = ea.indicator.IGD(pop.ObjV, PF)     # 计算IGD指标
             HV = ea.indicator.HV(NDSet.ObjV, PF)       # 计算HV指标
             logging.info('time: %s,  round %d/%d, IGD = %.7f, HV = %.7f' % (get_time(), i + 1, N, IGD, HV))
@@ -104,7 +102,7 @@ if __name__ == '__main__':
         # 保留xx个最好数据
         igd = np.sort(igd)[:10]  # igd取前xx
         hv = np.sort(hv)[-10:]  # hv取后xx
-        res = "median --- IGD={:.7f}, IGD.std={:.7f}, HV={:.7f}, HV.std={:.7f}".format(np.median(igd), np.std(igd), np.median(hv), np.std(hv))
+        res = "median --- IGD={:.7f}, IGD.std={:.7f}, HV={:.7f}, HV.std={:.7f}".format(np.mean(igd), np.std(igd), np.mean(hv), np.std(hv))
         results.append(res)
         logging.info(res)
     for i in range(len(problems)):
